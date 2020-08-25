644c9f0662ca75b365782cf51f48a3bfa5dd8574
Alexander Needham <alexanderneedham@alexandersair2.home>
2020-08-25 19:13:38 +0100

    cors fixx

--- server.js
+++ server.js
@@ -32,16 +32,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV !== "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://alex-trivia.herokuapp.com");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-  });
 }
 
 app.get("/", (req, res) => {
